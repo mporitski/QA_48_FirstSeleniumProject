72003b29737235c32e4cb700feb67bc38bc0c4cd
Margarita Poritski <mporitski@gmail.com>
2025-08-17 19:09:45 +0300

    add

--- src/test/java/com/telran/qa48/FirstSeleniumTest.java
+++ src/test/java/com/telran/qa48/FirstSeleniumTest.java
@@ -2,8 +2,11 @@ package com.telran.qa48;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstSeleniumTest {
 
@@ -13,16 +16,33 @@ public class FirstSeleniumTest {
     @BeforeMethod
     public void setUp(){
         driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        //maximize browser to window
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.tel-ran.com/");   //with history
+
+        //wait for all elements on site to load before starting test
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        //driver.get("https://www.tel-ran.com/");    //without history
+        //driver.navigate().to("https://www.google.com/");  //with history
+        driver.navigate().to("https://www.google.com/");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
     }
 
-    //test      -
+    //test
     @Test
     public void openGoogleTest(){
         System.out.println("Google opened!");
     }
 
     //after     -tearDown
-
+    @AfterMethod(enabled = true)
+    public void tearDown(){
+        //driver.quit();     //all tabs and browser
+        driver.close();      //only one tab
+    }
 
 }
