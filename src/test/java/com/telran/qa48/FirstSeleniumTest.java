6e6ac8b4190c2a2faeeb621a55ccf98939e2add7
Margarita Poritski <mporitski@gmail.com>
2025-08-17 13:34:32 +0300

    Initial commit

---
+++ src/test/java/com/telran/qa48/FirstSeleniumTest.java
@@ -0,0 +1,28 @@
+package com.telran.qa48;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver;
+
+    //before    -setUp
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+
+    //test      -
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Google opened");
+    }
+
+    //after     -tearDown
+
+
+}
